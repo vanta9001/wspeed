a4c5fec4fc6ba97de1dd39c1f13b9873b87ada29
thefriendlyhacker <bzogo7@gmail.com>
2025-09-19 10:58:18 +0000

    initial website clone

---
+++ js/i.js
@@ -0,0 +1,66 @@
+(function() { function bxBootstrap() {
+	var re = /bot|crawl|slurp|spider|mediapartners|headlesschrome|snap-prefetch|remotasks|woorank|uptime\.com|facebookexternalhit|facebookcatalog/i;
+	if (re.test(navigator.userAgent) || navigator.userAgent == '') {
+		return;
+	}
+
+	if (!(window.bouncex&&bouncex.website)) {
+		var pushedData = [];
+		if(window.bouncex && bouncex.push && bouncex.length){
+			pushedData = bouncex;
+		}
+		window.bouncex = {};
+		bouncex.pushedData = pushedData;
+		bouncex.website = {"id":8460,"name":"Literally Media | Know Your Meme","cookie_name":"bounceClientVisit8460","domain":"knowyourmeme.com","ct":"bind_to_domain","ally":0,"ei":0,"tcjs":"","cjs":"","force_https":false,"waypoints":false,"content_width":900,"gai":"","swids":"","sd":0,"ljq":"auto","campaign_id":0,"is_preview":false,"aco":{"first_party_limit":"3500","local_storage":"1"},"cmp":{"gdpr":1,"gmp":0,"whitelist_check":0},"burls":[],"ple":false,"fbe":true,"ffs":"","mas":2,"map":1,"gar":true,"ete":2,"ettm":true,"etjs":"","dge":true,"bxidLoadFirst":false,"pie":false,"cme":false,"gbi_enabled":1,"bpush":false,"pt":null,"els":{},"vars":[],"dgu":"pixel.cdnwidget.com","dgp":false,"ba":{"enabled":0,"fbte":0},"biu":"assets.bounceexchange.com","bau":"api.bounceexchange.com","beu":"events.bouncex.net","ibx":{"tjs":"","cjs":"","miw":0,"mibcx":1,"te":0,"cart_rep":{"get":"","set":""},"ulpj":null,"cus":"","miw_exclude":"","enabled":0},"etjson":null,"osre":true,"osru":"osr.bounceexchange.com/v1/osr/items","checkDfp":false,"gamNetwork":"","spa":0,"spatm":1,"preinit_cjs":"","crs":{"integrations":null,"pageCount":null},"mat":0,"math":0,"cpnu":"coupons.bounceexchange.com","dfpcms":0,"sms":{"optm":"","eventSharing":false,"shqId":"","enabled":0},"pde":true,"fmc":["AF","AX","AL","DZ","AS","AD","AO","AI","AQ","AG","AR","AM","AW","AP","AU","AT","AZ","BS","BH","BD","BB","BY","BE","BZ","BJ","BM","BT","BO","BQ","BA","BW","BV","BR","IO","BN","BG","BF","BI","CV","KH","CM","CA","KY","CF","TD","CL","CN","CX","CC","CO","KM","CG","CD","CK","CR","CI","HR","CU","CW","CY","CZ","DK","DJ","DM","DO","EC","EG","SV","GQ","ER","EE","SZ","ET","EU","FK","FO","FJ","FI","FR","GF","PF","TF","GA","GM","GE","DE","GH","GI","GR","GL","GD","GP","GU","GT","GG","GN","GW","GY","HT","HM","VA","HN","HK","HU","IS","IN","ID","IR","IQ","IE","IM","IL","IT","JM","JP","JE","JO","KZ","KE","KI","KP","KR","KW","KG","LA","LV","LB","LS","LR","LY","LI","LT","LU","MO","MK","MG","MW","MY","MV","ML","MT","MH","MQ","MR","MU","YT","MX","FM","MD","MC","MN","ME","MS","MA","MZ","MM","NA","NR","NP","NL","NC","NZ","NI","NE","NG","NU","NF","MP","NO","OM","PK","PW","PS","PA","PG","PY","PE","PH","PN","PL","PT","PR","QA","RE","RO","RU","RW","BL","SH","KN","LC","MF","PM","VC","WS","SM","ST","SA","SN","RS","SC","SL","SG","SX","SK","SI","SB","SO","ZA","GS","SS","ES","LK","SD","SR","SJ","SE","CH","SY","TW","TJ","TZ","TH","TL","TG","TK","TO","TT","TN","TR","TM","TC","TV","UG","UA","AE","GB","US","UM","UY","UZ","VU","VE","VN","VG","VI","WF","EH","YE","ZM","ZW"],"fme":true,"fmx":"","uid2":true,"sdk":{"android":{"enabled":false,"enabledVersions":[],"eventModifications":null},"ios":{"enabled":false,"enabledVersions":[],"eventModifications":null}},"onsite":{"enabled":1},"ads":{"enabled":1,"schainSellerDomain":"pmc.com","schainSellerId":"1226456"},"pubs":{"enabled":1},"websdk":{"enabled":0,"devMode":0},"ga4_property_id":"","ga4_measurement_id":"","tag_state_domain":"","tag_state_domain_enabled":false}
+;
+
+		bouncex.tag = 'tag3';
+		bouncex.$ = window.jQuery;
+		bouncex.env = 'production';
+		bouncex.restrictedTlds = {"casl":{"ca":1},"gdpr":{"ad":1,"al":1,"at":1,"ax":1,"ba":1,"be":1,"bg":1,"by":1,"xn--90ais":1,"ch":1,"cy":1,"cz":1,"de":1,"dk":1,"ee":1,"es":1,"eu":1,"fi":1,"fo":1,"fr":1,"uk":1,"gb":1,"gg":1,"gi":1,"gr":1,"hr":1,"hu":1,"ie":1,"im":1,"is":1,"it":1,"je":1,"li":1,"lt":1,"lu":1,"lv":1,"mc":1,"md":1,"me":1,"mk":1,"xn--d1al":1,"mt":1,"nl":1,"no":1,"pl":1,"pt":1,"ro":1,"rs":1,"xn--90a3ac":1,"ru":1,"su":1,"xn--p1ai":1,"se":1,"si":1,"sj":1,"sk":1,"sm":1,"ua":1,"xn--j1amh":1,"va":1,"tr":1}};
+		bouncex.client = {
+			supportsBrotli: 1
+		};
+		bouncex.assets = {"ads":"6a37bbf49f24c026647901c0af1b789e","creativesBaseStyles":"a53944a2","gpsAuction":"bbb80866120d17013073bb6d284cbd6b","inbox":"80c5f44531fa694e643c58392143dd81","onsite":"c05f8c5551fa6b964660ad61916291c1","sms":"e39203556bab2366e56296ce42e974a7","websdk":"9a65ab96520f390bb65a5ecce3f5ef5a","website_campaigns_8460":"26a43ec8329522c019b0dfa497175b52"};
+		bouncex.push = function(pushData) {
+			bouncex.pushedData.push(pushData);
+		}
+
+		var runtime = document.createElement('script');
+		runtime.setAttribute('src', '//assets.bounceexchange.com/assets/smart-tag/versioned/runtime_c81e76ee00d795b1eebf8d27949f8dc5.br.js');
+		runtime.setAttribute('async', 'async');
+
+		bouncex.initializeTag = function() {
+			var script = document.createElement('script');
+			script.setAttribute('src', '//assets.bounceexchange.com/assets/smart-tag/versioned/main-v2_b1827fb2f41236abce8b98acd44d9982.br.js');
+			script.setAttribute('async', 'async');
+			document.body.appendChild(script);
+
+
+			var deviceGraphScript = document.createElement('script');
+			deviceGraphScript.setAttribute('src', '//assets.bounceexchange.com/assets/smart-tag/versioned/cjs_min_c84323e2726f3e99b307ab7740c6434b.js');
+			deviceGraphScript.setAttribute('async', 'async');
+			var dgAttrs = [{"Key":"id","Value":"c.js"},{"Key":"async","Value":"true"},{"Key":"data-apikey","Value":"2^HIykD"},{"Key":"data-cb","Value":"bouncex.dg.initPostDeviceGraph"},{"Key":"data-bx","Value":"1"},{"Key":"data-adcb","Value":"bouncex.dg.getAdsOptStatus"}];
+			if (dgAttrs) {
+				for (var i = 0; i < dgAttrs.length; i++) {
+					deviceGraphScript.setAttribute(dgAttrs[i].Key, dgAttrs[i].Value);
+				}
+			}
+			document.body.appendChild(deviceGraphScript);
+
+			bouncex.initializeTag = function() {};
+		};
+
+		runtime.onload = bouncex.initializeTag;
+		document.body.appendChild(runtime);
+
+	}
+
+
+}
+
+if (document.readyState === "loading") {
+	document.addEventListener("DOMContentLoaded", bxBootstrap);
+} else {
+	bxBootstrap();
+}})();
